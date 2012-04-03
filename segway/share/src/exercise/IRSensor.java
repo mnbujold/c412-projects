b780bacfa35997d6f2433d5b278d4a1f0430183e
Mike Bujold <mnbujold@ualberta.ca>
2012-04-03 16:00:03 -0600

    IR file read

--- segway/share/src/exercise/IRSensor.java
+++ segway/share/src/exercise/IRSensor.java
@@ -1,5 +1,10 @@
 package exercise;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 import model.scene.SceneModel;
 import model.scene.SceneModelObject;
 import model.sensor.DistanceSensor;
@@ -15,6 +20,44 @@ public class IRSensor extends DistanceSensor
     public IRSensor(DistanceSensorConfig cfg, SceneModel scene)
     {
         super (cfg, scene);
+        
+        try{
+        	  // Open the file that is the first 
+        	  // command line parameter
+        	  //FileInputStream fstream = new FileInputStream("model.txt");
+        	  // Get the object of DataInputStream
+        	  //DataInputStream in = new DataInputStream(fstream);
+          	FileInputStream fstream = new FileInputStream("../share/cfg/model.txt");
+          	DataInputStream in = new DataInputStream(fstream);
+          	
+//        	File aFile=new File("../share/cfg/model.txt");
+          	BufferedReader br = new BufferedReader(new InputStreamReader(in));
+          	String strLine;
+          	String[] strs;
+          	Double real;
+          	Double mean;
+          	Double var;
+          	//Read File Line By Line
+          	while ((strLine = br.readLine()) != null) {
+          		strs = strLine.split(" ");
+          		System.out.println(strLine);
+          		if(strs.length==3){
+          			 System.out.println(strs[0]+strs[1]+strs[2]);
+          			 real = new Double(strs[0]);
+          			 mean = new Double(strs[1]);
+          			 var = new Double(strs[2]);
+          			 //Object j =realtoMean.put(real, mean);
+          			 //realtoMean.add(mean);
+          			 //j=realtoVariance.put(real, var);
+//            			 System.out.println("here2");
+          		}
+          	}
+        	  //Close the input stream
+          	in.close();
+          }catch (Exception e){//Catch exception if any
+          	System.err.println("Error: " + e.getMessage());
+          }
+
     }
     
     @Override
